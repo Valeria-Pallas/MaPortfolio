d7531a4aaad56fbddcd0d6ec97ac61d0275bd6ac
TanNguyen <phuctan3108@gmail.com>
2023-12-05 20:51:56 +0100

    Correct JPQL queries for user-project join table

--- src/main/java/com/example/repository/ProjectRepository.java
+++ src/main/java/com/example/repository/ProjectRepository.java
@@ -10,6 +10,6 @@ import com.example.models.Project;
 public interface ProjectRepository extends JpaRepository<Project, Integer>{
   List<Project> findByName(String name);
   List<Project> findByNameContaining(String name);
-  @Query("SELECT p FROM Project p JOIN p.list_of_users u WHERE u.id=:id_user")
+  @Query("SELECT p FROM Project p JOIN p.users u WHERE u.id=:id_user")
   List<Project> findByUserId(int id_user);
 }
