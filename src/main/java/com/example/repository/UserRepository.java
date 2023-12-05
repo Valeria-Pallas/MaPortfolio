3ed69b1072005e83dd41a4118ffee580a12d2dd5
BenoitRichet <149505830+BenoitRichet@users.noreply.github.com>
2023-12-05 13:44:01 +0100

    Revert "Enum"

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.models.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
-//  List<User> FindByName(String name);
-//  List<User> findByNameContaining(String name);
-	@Query("SELECT u FROM User u JOIN u.list_of_projects p WHERE p.id=:id_proj")
-	List<User> findByProjectId(int id_proj);
+  List<User> FindByName(String name);
+  List<User> findByNameContaining(String name);
+  @Query("SELECT u FROM User u JOIN u.list_of_projects p WHERE p.id=:id_proj")
+  List<User> findByProjectId(int id_proj);
 }
