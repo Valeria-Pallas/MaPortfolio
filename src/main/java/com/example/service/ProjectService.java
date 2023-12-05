36c323ed9f3d14df759c55e9de1ed2925c95ad40
TanNguyen <phuctan3108@gmail.com>
2023-12-05 19:26:04 +0100

    Add interfaces for project and user services
    + Improve clarity of variable names to follow camel case convention
    + Change return type (void to boolean) to better reflect the logic
    of the operations within these methods
    + Rename methods for better

--- src/main/java/com/example/service/ProjectService.java
+++ src/main/java/com/example/service/ProjectService.java
@@ -1,108 +1,71 @@
 package com.example.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.models.Project;
 import com.example.models.Task;
 import com.example.models.User;
-import com.example.repository.ProjectRepository;
 
 @Service
-public class ProjectService {
-  @Autowired
-  private ProjectRepository proj_repo;
+public interface ProjectService {
 
   /**
    * Get all projects in database
    * @return list of projects
    */
-  public List<Project> getAllProjects() {
-    return proj_repo.findAll();
-  }
+ List<Project> getAllProjects();
 
   /**
    * Get a project by project id
    * @param id project id
    * @return project with this id, null if not found
    */
-  public Project getProjectById(int id) {
-    return proj_repo.findById(id).orElse(null);
-  }
+  Project getProjectById(int id);
 
   /**
    * Update project having a given id with info from user-defined input
    * Do nothing if no project with id is found
-   * @param id project id
-   * @param new_project info of project to be update
+   * @param newProject info of project to be update
+   * @return true if project is updated successfully, false otherwise
    */
-  public void updateProjectById(int id, Project new_project) {
-    Project project = proj_repo.findById(id).orElse(null);
-    if (project != null) {
-      project.copyExceptIdFrom(new_project);
-      proj_repo.save(project);
-    }
-  }
+  boolean updateProject(Project newProject);
 
   /**
    * Add a new project to database if no project with the same id exists in database
-   * @param new_project new project
+   * @param newProject new project
+   * @return true if project is added successfully, false otherwise
    */
-  public void addProject(Project new_project) {
-    Project project_exist = proj_repo.findById(new_project.getId()).orElse(null);
-    if (project_exist == null) {
-      proj_repo.save(new_project);
-    }
-  }
+  boolean addProject(Project newProject);
 
   /**
    * Delete a project with a given id
    * @param id project id
+   * @return true if project is deleted successfully, false otherwise
    */
-  public void deleteProjectById(int id) {
-    proj_repo.deleteById(id);
-  }
+  boolean deleteProjectById(int id);
 
   /**
    * Add a new user to list of users of a project with a given id
-   * @param new_user new user
-   * @param id_project project id
+   * @param newUser new user
+   * @param projectId project id
+   * @return true if user is added to project successfully, false otherwise
    */
-  public void addUserToProject(User new_user, int id_project) {
-    Project project = proj_repo.findById(id_project).orElse(null);
-    if (project != null) {
-      project.addUser(new_user);
-      proj_repo.save(project);
-    }
-  }
+  boolean addUserToProject(User newUser, int projectId);
 
   /**
    * Add a task to list of tasks of a project with a given id
-   * @param new_task new task
-   * @param id_project project id
+   * @param newTask new task
+   * @param projectId project id
+   * @return true if task is added to project successfully, false otherwise
    */
-  public void addTaskToProject(Task new_task, int id_project) {
-    Project project = proj_repo.findById(id_project).orElse(null);
-    if (project != null) {
-      project.addTask(new_task);
-      proj_repo.save(project);
-    }
-  }
+  boolean addTaskToProject(Task newTask, int projectId);
 
   /**
    * Get all users of a project with a given id
    * @param id project id
    * @return list of all users
    */
-  public List<User> getAllUsersByProjectId(int id) {
-    Project project = proj_repo.findById(id).orElse(null);
-    List<User> users = new ArrayList<User>();
-    if (project != null) {
-      users = project.getListOfUsers();
-    }
-    return users;
-  }
+  List<User> getAllUsersByProjectId(int id);
 }
