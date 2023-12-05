b37fce4c0eefbd6ef9886f8a82d85a22fcd59e24
TanNguyen <phuctan3108@gmail.com>
2023-12-05 14:19:23 +0100

    Add annotations to simplify classes

--- src/main/java/com/example/models/User.java
+++ src/main/java/com/example/models/User.java
@@ -11,9 +11,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "user_portfolio")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	@Id
@@ -29,13 +39,10 @@ public class User {
 	private String password;
 	
 	@OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST)
-	private List<Task> list_of_tasks;
+	private List<Task> tasks;
 	
-	@ManyToMany(mappedBy = "list_of_users")
-	private List<Project> list_of_projects;
-
-	public User() {
-	}
+	@ManyToMany(mappedBy = "users")
+	private List<Project> projects;
 
 	/**
 	 * Constructor using user inputs
@@ -66,127 +73,6 @@ public class User {
 	}
 
 	/**
-	 * Constructor using user inputs
-	 * 
-	 * @param id       : ID for user
-	 * @param name     : user name
-	 * @param email    : email address
-	 * @param password : user password
-	 */
-	public User(int id, String name, String email, String password, List<Task> task_list, List<Project> project_list) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.list_of_tasks = task_list;
-		this.list_of_projects = project_list;
-	}
-
-	/**
-	 * Get user id
-	 * 
-	 * @return user id
-	 */
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * Get user name
-	 * 
-	 * @return user name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * Get user's email
-	 * 
-	 * @return user's email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * Get user's password
-	 * @return user's password
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-	/**
-	 * Get list of tasks
-	 * 
-	 * @return list of tasks
-	 */
-	public List<Task> getListOfTasks() {
-		return list_of_tasks;
-	}
-
-	/**
-	 * Get list of projects
-	 * 
-	 * @return list of projects
-	 */
-	public List<Project> getListOfProjects() {
-		return list_of_projects;
-	}
-
-	/**
-	 * Set new id of user
-	 * @param id : new id
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	/**
-	 * Set new name for user
-	 * @param name : new name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * Set new email of user
-	 * @param email : new email
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * Set new password
-	 * 
-	 * @param new_pwd : new password
-	 */
-	public void setPassword(String new_pwd) {
-		this.password = new_pwd;
-	}
-
-	/**
-	 * Set list of tasks
-	 * 
-	 * @param task_list : list of tasks
-	 */
-	public void setListOfTasks(List<Task> task_list) {
-		this.list_of_tasks = task_list;
-	}
-
-	/**
-	 * Set list of projects
-	 * 
-	 * @param project_list : list of projects
-	 */
-	public void setListOfProjects(List<Project> project_list) {
-		this.list_of_projects = project_list;
-	}
-
-	/**
 	 * Copy new info, except user's id, to this user 
 	 * @param new_user new info of user
 	 */
@@ -194,8 +80,8 @@ public class User {
 		this.name = new_user.getName();
 		this.email = new_user.getEmail();
 		this.password = new_user.getPassword();
-		this.list_of_tasks = new_user.getListOfTasks();
-		this.list_of_projects = new_user.getListOfProjects();
+		this.tasks = new_user.getTasks();
+		this.projects = new_user.getProjects();
 	}
 
 	/**
@@ -204,8 +90,8 @@ public class User {
 	 * @param new_task new task
 	 */
 	public void addTask(Task new_task) {
-		if (!list_of_tasks.stream().anyMatch(task -> task.getId() == new_task.getId())) {
-			list_of_tasks.add(new_task);
+		if (!tasks.stream().anyMatch(task -> task.getId() == new_task.getId())) {
+			tasks.add(new_task);
 		}
 	}
 
@@ -215,9 +101,9 @@ public class User {
 	 * @param new_task new info of task
 	 */
 	public void updateTaskById(int id, Task new_task) {
-		for (int i=0; i<list_of_tasks.size(); i++) {
-			if (list_of_tasks.get(i).getId() == id) {
-				list_of_tasks.get(i).copyExceptIdFrom(new_task);
+		for (int i=0; i<tasks.size(); i++) {
+			if (tasks.get(i).getId() == id) {
+				tasks.get(i).copyExceptIdFrom(new_task);
 				break;
 			}
 		}
@@ -228,7 +114,7 @@ public class User {
 	 * @param id id of task to be deleted
 	 */
 	public void deleteTaskById(int id) {
-		list_of_tasks.removeIf(task -> task.getId() == id);
+		tasks.removeIf(task -> task.getId() == id);
 	}
 
 	/**
@@ -237,8 +123,8 @@ public class User {
 	 * @param new_project new project
 	 */
 	public void addProject(Project new_project) {
-		if (!list_of_projects.stream().anyMatch(user -> user.getId() == new_project.getId())){
-			list_of_projects.add(new_project);
+		if (!projects.stream().anyMatch(user -> user.getId() == new_project.getId())){
+			projects.add(new_project);
 		}
 	}
 
@@ -248,9 +134,9 @@ public class User {
 	 * @param new_project new info of project
 	 */
 	public void updateProjectById(int id, Project new_project) {
-		for (int i=0; i<list_of_projects.size(); i++) {
-			if (list_of_projects.get(i).getId() == id) {
-				list_of_projects.get(i).copyExceptIdFrom(new_project);
+		for (int i=0; i<projects.size(); i++) {
+			if (projects.get(i).getId() == id) {
+				projects.get(i).copyExceptIdFrom(new_project);
 				break;
 			}
 		}
@@ -261,7 +147,7 @@ public class User {
 	 * @param id id of project to be deleted
 	 */
 	public void deleteProjectById(int id) {
-		list_of_projects.removeIf(project -> project.getId() == id);
+		projects.removeIf(project -> project.getId() == id);
 	}
 
 	@Override
