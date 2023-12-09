f3a2548bd6cfb932fefb89dfbd87bfb688fcec3b
TanNguyen <phuctan3108@gmail.com>
2023-12-09 20:12:13 +0100

    Add missing annotations @Service  @Transactional

--- src/main/java/com/example/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/example/service/impl/ProjectServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.models.Project;
 import com.example.models.Task;
@@ -13,6 +14,7 @@ import com.example.repository.ProjectRepository;
 import com.example.service.ProjectService;
 
 @Service
+@Transactional
 public class ProjectServiceImpl implements ProjectService {
   @Autowired
   private ProjectRepository projRepo;
