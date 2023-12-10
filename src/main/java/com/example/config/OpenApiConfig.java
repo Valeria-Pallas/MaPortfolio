829e2b9ecf5a9a5e2c5189279ce16198d82d0668
Valeria-Pallas <valerianpallas@gmail.com>
2023-12-10 15:14:38 +0100

    resoudre conflit OpenApiConfig

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -8,11 +9,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
-
+@Configuration
 public class OpenApiConfig {
 
   @Bean
   public OpenAPI customOpenAPI() {
+
     final String SecuritySchemeName = "bearerAuth";
     final String apiTitle = "Portfolio API";
     final String apiDescription = "API for Portfolio";
