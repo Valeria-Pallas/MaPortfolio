84785f7d3b6e87c62cf3af4fa03ca98b6be9e000
TanNguyen <phuctan3108@gmail.com>
2023-12-11 21:13:43 +0100

    Apply correction to the mapper interfaces
    Include three interfaces: UserMapper, ProjectMapper, TaskMapper

--- src/main/java/com/example/util/UserMapper.java
+++ src/main/java/com/example/util/UserMapper.java
@@ -1,25 +1,10 @@
 package com.example.util;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import com.example.dto.UserDTO;
 import com.example.models.User;
 
-@Mapper
-public interface UserMapper {
-
-  UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
-  @Mapping(source = "tasks", target = "list_of_tasks")
-  @Mapping(source = "projects", target = "list_of_projects")
-  UserDTO userToUserDTO(User user);
-
-  @Mapping(source = "list_of_tasks", target = "tasks")
-  @Mapping(source = "list_of_projects", target = "projects")
-  @Mapping(target = "password", ignore = true)
-  @Mapping(target = "roles", ignore = true)
-  User userDTOToUser(UserDTO userDTO) throws Exception;
-
+@Mapper(componentModel = "spring")
+public interface UserMapper extends ApplicationMapper<User, UserDTO>{
 }
